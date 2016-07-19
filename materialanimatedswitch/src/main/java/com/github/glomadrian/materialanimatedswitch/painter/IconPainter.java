65e849c5e5ce6dd4af9bb4405a77706b425017ba
aardouin <alexis.ardouin@gmail.com>
2016-07-19 11:04:16 +0200

    Allow ball size param

--- materialanimatedswitch/src/main/java/com/github/glomadrian/materialanimatedswitch/painter/IconPainter.java
+++ materialanimatedswitch/src/main/java/com/github/glomadrian/materialanimatedswitch/painter/IconPainter.java
@@ -3,7 +3,9 @@ package com.github.glomadrian.materialanimatedswitch.painter;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+
 import com.github.glomadrian.materialanimatedswitch.R;
 
 /**
@@ -11,46 +13,49 @@ import com.github.glomadrian.materialanimatedswitch.R;
  */
 public abstract class IconPainter implements SwitchInboxPinnedPainter {
 
-  protected Bitmap iconBitmap;
-  protected Context context;
-  protected Paint paint;
-  protected int width;
-  protected int height;
-  protected int imageHeight;
-  protected int imageWidth;
-  protected boolean isVisible = false;
-  protected int iconXPosition;
-  protected int iconYPosition;
-  protected int margin;
+    protected Bitmap iconBitmap;
+    protected Context context;
+    protected Paint paint;
+    protected int width;
+    protected int height;
+    protected int imageHeight;
+    protected int imageWidth;
+    protected boolean isVisible = false;
+    protected int iconXPosition;
+    protected int iconYPosition;
+    protected int margin;
 
-  public IconPainter(Context context, Bitmap bitmap, int margin) {
-    this.context = context;
-    this.iconBitmap = bitmap;
-    this.margin = margin;
-    init();
-  }
-
-  private void init() {
-    paint = new Paint();
-    paint.setAntiAlias(true);
-    initBitmap();
-  }
-
-  protected void initBitmap() {
-    int iconSize = (int) context.getResources().getDimension(R.dimen.icon_size);
-    iconBitmap = Bitmap.createScaledBitmap(iconBitmap, iconSize, iconSize, false);
-    imageHeight = iconBitmap.getHeight();
-    imageWidth = iconBitmap.getWidth();
-  }
-
-  @Override public void draw(Canvas canvas) {
-    if (isVisible) {
-      canvas.drawBitmap(iconBitmap, iconXPosition, iconYPosition, paint);
+    public IconPainter(Context context, Bitmap bitmap, int margin) {
+        this.context = context;
+        this.iconBitmap = bitmap;
+        this.margin = margin;
+        init();
     }
-  }
 
-  @Override public void onSizeChanged(int height, int width) {
-    this.height = height;
-    this.width = width;
-  }
+    private void init() {
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(Color.RED);
+        initBitmap();
+    }
+
+    protected void initBitmap() {
+        int iconSize = (int) context.getResources().getDimension(R.dimen.icon_size);
+        iconBitmap = Bitmap.createScaledBitmap(iconBitmap, iconSize, iconSize, false);
+        imageHeight = iconBitmap.getHeight();
+        imageWidth = iconBitmap.getWidth();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        if (isVisible) {
+            canvas.drawBitmap(iconBitmap, iconXPosition, iconYPosition, paint);
+        }
+    }
+
+    @Override
+    public void onSizeChanged(int height, int width) {
+        this.height = height;
+        this.width = width;
+    }
 }
